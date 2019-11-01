3192c95442273176cdbce67813bdda6c648cb243
SuHaLee <gimtommang88@gmail.com>
2019-11-02 04:18:20 +0900

    Modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SideBar from './component/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <div>hello</div>
-    </div>
+    <>
+      <div>test</div>
+      <SideBar></SideBar>
+    </>
   );
 }
 
