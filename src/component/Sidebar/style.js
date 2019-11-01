76650a651ef57faf1a998a117daaa9148b3811a4
SuHaLee <gimtommang88@gmail.com>
2019-11-02 06:26:48 +0900

    create sidebar style.js

---
+++ src/component/Sidebar/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  display: table-cell;
+  vertical-align: middle;
+  height: 12.5rem;
+  color: #ffff;
+  font-size: 1.563rem;
+  text-align: center;
+`
+
+export const SideBarContainer = styled.div`
+  width: 21.19rem;
+  height: 100vh;
+  background-color: #50505D;
+  
+` 
