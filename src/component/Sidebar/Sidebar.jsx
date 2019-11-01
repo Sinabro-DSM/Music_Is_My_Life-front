116b20c11924e37303a249a6b0f0a7c921321e34
SuHaLee <gimtommang88@gmail.com>
2019-11-02 04:19:39 +0900

    create sidebar.jsx

---
+++ src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SidebarItems from "./SidebarItems/SidebarItems";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SidebarItems></SidebarItems>
+    </div>
+  );
+};
+
+export default Sidebar;
