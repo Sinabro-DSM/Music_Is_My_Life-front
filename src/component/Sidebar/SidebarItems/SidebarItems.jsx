10247b6ad1e6a17cea580975791ef20d21196485
SuHaLee <gimtommang88@gmail.com>
2019-11-02 04:46:25 +0900

    CREATE sidebaritems

---
+++ src/component/Sidebar/SidebarItems/SidebarItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './style';
+
+const SidebarItems = () => {
+  return (
+    <>
+      <S.SidebarButton>Drum</S.SidebarButton>
+      <S.SidebarButton>Jang-Gu</S.SidebarButton>
+      <S.SidebarButton>Recorder</S.SidebarButton>
+    </>
+  );
+};
+
+export default SidebarItems;
